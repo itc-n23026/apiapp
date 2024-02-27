9de564b8a5a33171d269fa2497c74f22aa9a6eb4
itc-n23026 <n23026@std.it-college.ac.jp>
2024-02-27 08:59:17 +0000

    name japanese ++
    ポケモン名を日本語表記に、英語名を調べるためのサイトへのリンクを追加

--- pages/index.js
+++ pages/index.js
@@ -5,43 +5,46 @@ const Pokemon = () => {
   const [pokemonData, setPokemonData] = useState(null)
   const [pokemonName, setPokemonName] = useState('')
 
-  useEffect(() => {
-    const getPokemonData = async () => {
-      try {
-        const englishName = await getEnglishName(pokemonName)
-        const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${englishName.toLowerCase()}`
-        )
-        if (!response.ok) {
-          throw new Error('Network response was not ok')
-        }
-        const data = await response.json()
-        setPokemonData(data)
-      } catch (error) {
-        console.error('Error fetching Pokemon data:', error)
-      }
-    }
-
-    getPokemonData()
-  }, [pokemonName])
-
-  const handleInputChange = event => {
-    setPokemonName(event.target.value)
-  }
-
-  const getEnglishName = async japaneseName => {
+  const loadPokemonData = async () => {
     try {
       const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon-species/${japaneseName.toLowerCase()}`
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
       )
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
       const data = await response.json()
-      return data.name
+      setPokemonData(data)
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error)
+    }
+  }
+
+  const loadPokemonName = async englishName => {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${englishName.toLowerCase()}`
+      )
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      const japaneseName = data.names.find(name => name.language.name === 'ja')
+        .name
+      setPokemonName(japaneseName)
     } catch (error) {
       console.error('Error fetching Pokemon species data:', error)
-      return ''
+    }
+  }
+
+  const handleInputChange = event => {
+    setPokemonName(event.target.value)
+  }
+
+  const handleButtonClick = () => {
+    if (pokemonName !== '') {
+      loadPokemonName(pokemonName)
+      loadPokemonData()
     }
   }
 
@@ -52,20 +55,18 @@ const Pokemon = () => {
           type='text'
           value={pokemonName}
           onChange={handleInputChange}
-          placeholder='ポケモンの名前か図鑑番号を入力'
+          placeholder='名前か図鑑番号を入力'
         />
+        <button onClick={handleButtonClick}>検索</button>
         {pokemonData ? (
           <div>
-            <h1>{pokemonData.name}</h1>
+            <h1>{pokemonName}</h1>
             {pokemonData.sprites && pokemonData.sprites.front_default ? (
-              <img
-                src={pokemonData.sprites.front_default}
-                alt={pokemonData.name}
-              />
+              <img src={pokemonData.sprites.front_default} alt={pokemonName} />
             ) : (
-              <p>画像はありません</p>
+              <p>画像が見つかりません</p>
             )}
-            <h2>ステータス:</h2>
+            <h2>ステータス</h2>
             <ul>
               {pokemonData.stats &&
                 pokemonData.stats.map(stat => (
@@ -89,10 +90,20 @@ const Pokemon = () => {
             </ul>
           </div>
         ) : pokemonName !== '' ? (
-          <p>読み込み中...</p>
+          <p>Loading...</p>
         ) : (
-          <p>ポケモンの名前、または図鑑番号を入力してください</p>
+          <p>ポケモンの名前か図鑑番号を入力して検索できます</p>
         )}
+        <p>
+          ポケモンの英語名の確認は
+          <a
+            href='https://wiki.xn--rckteqa2e.com/wiki/%E3%83%9D%E3%82%B1%E3%83%A2%E3%83%B3%E3%81%AE%E5%A4%96%E5%9B%BD%E8%AA%9E%E5%90%8D%E4%B8%80%E8%A6%A7'
+            target='_blank'
+          >
+            コチラ
+          </a>
+          。
+        </p>
       </div>
       {pokemonData && pokemonData.stats && pokemonData.stats.length > 0 && (
         <div style={{ marginLeft: 'auto', flex: 1 }}>
